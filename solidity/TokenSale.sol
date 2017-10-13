c2f5afe44d6635399211b6ecaaf79dc4386c5499
Dax Ravi <d@ironbay.co>
2017-10-13 13:35:43 -0400

    Token sale contract

--- solidity/TokenSale.sol
+++ solidity/TokenSale.sol
@@ -5,11 +5,37 @@ import "./Token.sol";
 contract TokenSale {
     Token public token;
     address public owner;
+    uint256 public rate;
+    uint256 public raised;
+    uint256 public sold;
 
-    function TokenSale(address _token) public payable {
+    function TokenSale(address _token, uint256 _rate) public payable {
         token = Token(_token);
         owner = msg.sender;
+        setRate(_rate);
     }
 
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function setRate(uint256 _rate) public onlyOwner {
+        rate = 1000000000000000000 / _rate;
+    }
+
+    function () payable {
+        require(valid());
+        uint256 tokens = msg.value / rate;
+        require(tokens > 0);
+        token.transferFrom(owner, msg.sender, tokens);
+        owner.transfer(msg.value);
+        raised = raised + msg.value;
+        sold = sold + tokens;
+    }
+
+    function valid() internal constant returns (bool) {
+        return msg.value != 0;
+    }
 
 }
