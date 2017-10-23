f9b9a4fd4f1437c629081aa3fec93fe3c7a8a2a7
Dax Ravi <d@ironbay.co>
2017-10-23 10:46:47 -0400

    Fig

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -22,8 +22,8 @@ defmodule Stars.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:kora, github: "ironbay/kora"},
+      {:fig, github: "ironbay/fig"},
     ]
   end
 end
