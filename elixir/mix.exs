9f4be5afb59a4186f84987525fa1fbd46bfb1d60
Dax Ravi <d@ironbay.co>
2017-10-31 11:34:18 -0400

    Lock

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -24,7 +24,7 @@ defmodule Stars.MixProject do
     [
       {:kora, github: "ironbay/kora"},
       {:fig, github: "ironbay/fig"},
-      {:dynamic, github: "ironbay/dynamic_ex"},
+      {:dynamic_ex, github: "ironbay/dynamic_ex"},
       {:comeonin, "~> 3.0"},
     ]
   end
